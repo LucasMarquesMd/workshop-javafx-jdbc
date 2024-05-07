1b8f5c30f1277d9aa899418e79e38cab261e2b81
Lucas Marques Mendes <lucasmqmendes@gmail.com>
2024-05-06 23:47:15 -0300

    Adding database access

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.function.Consumer;
 
@@ -36,15 +37,15 @@ public class MainViewController implements Initializable{
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		loadView("/gui/DepartmentList.fxml",
-				//Acao de inicializacao do controle da classe de departamentos (funcao lambda)
 			(DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();
-		});
+		});//Acao de inicializacao do controle da classe de departamentos (funcao lambda)
 	}
+
 	
 	@FXML
-	public void onMenuItemAboutrAction() {
+	public void onMenuItoutemAbrAction() {
 		loadView("/gui/About.fxml", x -> {});//A tela About leva uma funcao lambda vazia pois nao precisa inicializar nada
 	}
 	
@@ -58,15 +59,23 @@ public class MainViewController implements Initializable{
 	//Funcao para abrir outras telas
 	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
+			 // Carrega o novo arquivo FXML que define a interface do usuário
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			// Carrega e instancia o novo layout principal da cena a partir do arquivo FXML
 			VBox newVBox = loader.load();
 			
+			//Cira uma nova cena
 			Scene mainScene = Main.getMainScene();
+			//Pega o conteudo dentro do ScrollPane da MainView - Pega todo o VBox 
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
+			//
+			//Salva apenas o filho menubar do VBox da MainView
 			Node mainMenu = mainVBox.getChildren().get(0);
+			//Limpa a tela princiapal - apaga os itens do VBox
 			mainVBox.getChildren().clear();
+			//Adiociona o menubar devolta a tela principal
 			mainVBox.getChildren().add(mainMenu);
+			//Adiciona os novos itens (filhos) da nova tela a tela principal - dentro do VBox
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
 			
